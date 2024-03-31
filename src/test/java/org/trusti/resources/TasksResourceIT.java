169850df8ed53adef4bd98612700dbabb6d30db3
carlosthe19916 <2582866+carlosthe19916@users.noreply.github.com>
2024-03-31 22:24:06 +0200

    Add TasksResourceIT

---
+++ src/test/java/org/trusti/resources/TasksResourceIT.java
@@ -0,0 +1,8 @@
+package org.trusti.resources;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class TasksResourceIT extends TasksResourceTest {
+    // Execute the same tests but in packaged mode.
+}
